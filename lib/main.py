a875f8b9b714b25161346e334e085a9c29d3006a
noanmee42 <zhuchatskiy+42@gmail.com>
2026-02-24 19:35:41 +0800

    nonbinary search

--- lib/main.py
+++ lib/main.py
@@ -10,12 +10,19 @@ def communisort_v2(arr):
     new_array = []
     return new_array
 
-# Nosort - who even cares about sorting? Let's agree that it's sorted. O(0)
+# No sort - who even cares about sorting? Let's agree that it's sorted. O(0)
 def nosort(arr):
     return arr
 
-# FastestUnifier_ofCommonKeylists (AKA "FUCK") - fastest sorting algorithm of all time.
+# Fastest Unifier of Common Keylists (AKA "FUCK") - fastest sorting algorithm of all time.
 # It's so fast u can't access its results. O(-1)
 def fuck(arr):
     pass
 
+# non-binary search - since everyone wants to have an identity. O(n)
+def nonbinary_search(arr):
+    nonbinaries = [6, 7, 42, 52, 67, 69, 161, 228, 420, 666, 777, 1161, 1337, 1488]
+    for i in range(len(arr)):
+        if arr[i] in nonbinaries:
+            return {arr[i]: i}
+    
