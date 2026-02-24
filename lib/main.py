f184c5850bc0b9dd7c06c9d59629747bca461f2b
noanmee42 <zhuchatskiy+42@gmail.com>
2026-02-24 19:15:03 +0800

    Communisort v2

--- lib/main.py
+++ lib/main.py
@@ -1,6 +1,11 @@
-# Communisort (v1) - everyone gets mean
+# Communisort (v1) - everyone gets mean. O(n)
 def communisort(arr):
     eq = round(sum(arr) / len(arr))
     for i in range(len(arr)):
         arr[i] = eq
     return arr
+
+# Communisort (v2) - everyone gets nothing. O(1)
+def communisort_v2(arr):
+    new_array = []
+    return new_array
