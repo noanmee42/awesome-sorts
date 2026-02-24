cae2bed674e6b5a4e8e57b193aa8dda84d5180b7
noanmee42 <zhuchatskiy+42@gmail.com>
2026-02-24 19:17:57 +0800

    nosort

--- lib/main.py
+++ lib/main.py
@@ -9,3 +9,9 @@ def communisort(arr):
 def communisort_v2(arr):
     new_array = []
     return new_array
+
+# Nosort - who even cares about sorting? Let's agree that it's sorted. O(0)
+def nosort(arr):
+    return arr
+
+# 
